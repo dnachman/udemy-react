4431607f572a64c73628f00a90b6b0106516fbd5
David Nachman <david@nachman.org>
2015-10-22 18:53:14 -0400

    initial

---
+++ thumbnail-gulp/src/app.jsx
@@ -0,0 +1,22 @@
+var options = {
+  thumbnailData: [{
+    title: 'See Tutorials',
+    number: 32,
+    header: 'Learn React',
+    description: 'React is a great new javascript library that does lots of stuff',
+    imageUrl: 'http://formatjs.io/img/react.svg'
+  },
+  {
+    title: 'Show courses',
+    number: 25,
+    header: 'Learn Gulp',
+    description: 'Gulp will speed up development workflow',
+    imageUrl: 'https://avatars0.githubusercontent.com/u/6200624?v=3&s=400'
+  }]
+};
+
+// react, please render this class
+var element = React.createElement(ThumbnailList, options);
+
+// react, after you render this, please place it in my body tag
+React.render(element, document.querySelector('.target'));
