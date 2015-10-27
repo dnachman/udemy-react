28d597660409df935a1b8100167f3aff858b3aaa
David Nachman <david@nachman.org>
2015-10-27 17:32:14 -0400

    End of section 4, dropdown complete

--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -1,18 +1,38 @@
 var react = require('react');
 var Button = require('./button');
-// var List = require('./list');
+var ListItem = require('./list-item');
 
 module.exports = React.createClass({
   handleClick: function() {
-    alert('hello from dropdown');
+    // forces re-render, don't directly manipulate this.state
+    this.setState({open: !this.state.open});
+  },
+  getInitialState: function() {
+    return { open: false };
+  },
+  handleItemClick: function(item) {
+    this.setState({
+      open: false,
+      itemTitle: item
+    });
   },
   render: function() {
+    var list = this.props.items.map(function(item) {
+      return <ListItem
+        item={item}
+        whenItemClicked={this.handleItemClick}
+        className={this.state.itemTitle === item ? "active" : ""}/>
+    }.bind(this));
     return <div className="dropdown">
       <Button
         whenClicked={this.handleClick}
         className="btn-default"
-        title={this.props.title}
+        title={this.state.itemTitle || this.props.title}
         subTitleClassName="caret" />
+
+      <ul className={"dropdown-menu " + (this.state.open ? "show" : "") }>
+        {list}
+      </ul>
 
     </div>
   }
