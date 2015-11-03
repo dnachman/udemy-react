ff5f9abc0908b7e4b4da805f09c4f6c07d2f9782
David Nachman <david@nachman.org>
2015-11-03 14:34:01 -0500

    End of lecture 30, basci return of data

--- todos/src/app.jsx
+++ todos/src/app.jsx
@@ -1,21 +1,33 @@
 var React = require('react');
 var ReactFire = require('reactfire');
 var Firebase = require('firebase');
+var Header = require('./header');
+var List = require('./list');
 var rootUrl = 'https://torrid-torch-1602.firebaseio.com/';
 
 var App = React.createClass({
   mixins: [ReactFire],
+  getInitialState: function() {
+    return {
+      items: {}
+    }
+  },
   componentWillMount: function() {
     // bind this object view ReactFire to this.state
     this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
   },
   render: function() {
 
-    console.log(this.state);
+    return <div className="row panel panel-default">
+      <div className="col-md-8 col-md-offset-2">
+        <h2 className="text-center">
+          To-Do List
+        </h2>
+        <Header itemsStore={this.firebaseRefs.items}/>
+        <List items={this.state.items} />
+      </div>
 
-    return <h1>
-      Hello!
-    </h1>
+    </div>
   }
 });
 
